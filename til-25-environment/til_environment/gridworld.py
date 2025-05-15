fix: off by one error in grid

til-25-environment/til_environment/gridworld.py
@@ -177,7 +177,7 @@ class raw_env(AECEnv[AgentID, ObsType, ActionType]):
                 "direction": Discrete(len(Direction)),
                 "scout": Discrete(2),
                 "location": Box(0, self.size, shape=(2,), dtype=np.uint8),
-                "step": Discrete(NUM_ITERS),
+                "step": Discrete(NUM_ITERS + 1),
             }
         )
 